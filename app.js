f880f296c00ae61ade097e68311ede11b1dca100
mordok22012 <yoelagustindorta@gmail.com>
2023-04-05 21:46:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('button');
+const body = document.querySelector('body');
+const colores = ['blue', 'yellow', 'green', 'black', 'white'];
+
+
+btn.addEventListener('click', fondo);
+
+function fondo() {
+    const indice = parseInt(Math.random() * colores.length);
+    body.style.backgroundColor = colores[indice];
+}
